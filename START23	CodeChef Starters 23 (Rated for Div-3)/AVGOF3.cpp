1fb58fece9f3ca5c42d4adb8fa400e351988f5e1
Rishaw Kumar <68506932+rishawsingh@users.noreply.github.com>
2022-01-29 22:59:17 +0530

    Create AVGOF3.cpp

---
+++ START23	CodeChef Starters 23 (Rated for Div-3)/AVGOF3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+	    int res=0;
+	    cin>>x;
+	    cout<<x-1<<" "<<x<<" "<<x+1<<endl;
+	    
+
+	}
+	
+	return 0;
+}
+A
